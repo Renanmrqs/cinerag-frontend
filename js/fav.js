ed88a63ec1d11b1515ee0cb5e491024058517284
Renanmrqs <renanmarques1923@gmail.com>
2026-06-02 13:00:59 -0300

    featch: nav bar e tela de favoritos

--- js/fav.js
+++ js/fav.js
@@ -12,6 +12,31 @@ async function show_favorites () {
     )
     const data = await res.json()
     console.log(data)
+    const favs_panel = document.querySelector('#favorites-panel') 
+
+    data.forEach(fav => {
+        const fav_card = document.createElement("div")
+        fav_card.classList.add("fav-card")
+
+        fav_card.innerHTML = `
+        <h2>${fav.title}</h2>
+
+        <span class="badge ${fav.sentiment}">
+        ${fav.sentiment} reviews
+        </span>
+
+        <p>Trust: ${(fav.trust * 100).toFixed(0)}%</p>
+        <div class="trust-bar">
+            <div class="trust-fill" style="width: ${fav.trust * 100}%"></div>
+        </div>
+
+        <span id="date" >Added on: ${fav.added_at}</span> 
+        <hr>
+        `
+
+        favs_panel.append(fav_card)
+    });
+
 }
 
 show_favorites()
